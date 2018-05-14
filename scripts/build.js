54b97b601abe96fae151f78289b4c1ede8d6f372
Georgios Eleftheriadis <georgaleos232@gmail.com>
2018-05-14 11:32:23 +0300

    Add functionality to upload docs to github pages

--- scripts/build.js
+++ scripts/build.js
@@ -24,7 +24,7 @@ const checkRequiredFiles = require('react-dev-utils/checkRequiredFiles');
 const formatWebpackMessages = require('react-dev-utils/formatWebpackMessages');
 const printHostingInstructions = require('react-dev-utils/printHostingInstructions');
 const FileSizeReporter = require('react-dev-utils/FileSizeReporter');
-const printBuildError = require('react-dev-utils/printBuildError');
+// const printBuildError = require('react-dev-utils/printBuildError');
 
 const measureFileSizesBeforeBuild =
   FileSizeReporter.measureFileSizesBeforeBuild;
@@ -95,7 +95,7 @@ measureFileSizesBeforeBuild(paths.appBuild)
     },
     err => {
       console.log(chalk.red('Failed to compile.\n'));
-      printBuildError(err);
+      // printBuildError(err);
       process.exit(1);
     }
   );
